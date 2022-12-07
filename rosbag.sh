4b81b4deea30efd3088103a3234e20e8cd5acf27
lennylee <qq1021620522@gmail.com>
2022-12-07 16:14:59 +0800

    1207 add power measure unit into platform

---
+++ rosbag.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /opt/ros/noetic/setup.bash
+source /home/car/omni-evaluation/devel/setup.bash
+rosbag record -o square-25 /odometry/filtered /cmd_state /cmd_vel /M3508_Rx_State /GM6020_Rx_State
